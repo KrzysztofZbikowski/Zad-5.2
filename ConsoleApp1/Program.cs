fab0144cfc577977854a8b04e1883a054eb76bb6
Zbiku <Zbiku@MSI>
2023-11-24 22:42:33 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿class zad
+{
+    static bool sprawdz(double a, double b, double x)
+    {
+        if (x > a && x < b)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    static void Main(string[] args)
+    {
+        double a, b, x;
+        Console.WriteLine("Podaj dolny zakres: ");
+        a = double.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj górny zakres: ");
+        b = double.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj twoją liczbę: ");
+        x = double.Parse(Console.ReadLine());
+        Console.WriteLine(sprawdz(a, b, x));
+        Console.ReadKey();
+    }
+}
